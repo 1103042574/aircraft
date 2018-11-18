90e89d206d530329b81a65434a6dad8e95cf64a5
root <1103042574@qq.com>
2018-11-18 13:05:09 +0800

    update

---
+++ aircraft.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/aircraft/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+	console.log(msg);
+  });
+});
+
+
+http.listen(4000, function(){
+  console.log('listening on *:4000');
+});
